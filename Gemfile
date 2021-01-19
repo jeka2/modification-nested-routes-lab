bf39e1de9f42a4dd0e471ce03ec9f19c3974e66b
Yevgeniy <anigeka@gmail.com>
2021-01-19 18:45:35 -0500

    Done.

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ group :development, :test do
   gem 'capybara'
   gem 'rspec-rails'
   gem 'shoulda'
+  gem 'pry'
 end
 
 group :development do
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,12 +25,21 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if params[:artist_id]
+      @artist = Artist.find_by_id(params[:artist_id])
+      if @artist 
+        @song = @artist.songs.new
+      else
+        flash[:alert] = "No such artist"
+        redirect_to artists_path 
+      end
+    else
+      @song = Song.new
+    end
   end
 
   def create
     @song = Song.new(song_params)
-
     if @song.save
       redirect_to @song
     else
@@ -39,7 +48,21 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      artist = Artist.find_by_id(params[:artist_id])
+      if artist
+        @song = artist.songs.find_by_id(params[:id])
+        unless @song
+          flash[:alert] = "Song not found"
+          redirect_to artist_songs_path(artist)
+        end
+      else
+        flash[:alert] = "Artist not found"
+        redirect_to artists_path
+      end
+    else
+      @song = Song.find(params[:id])
+    end
   end
 
   def update
@@ -64,7 +87,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
 
